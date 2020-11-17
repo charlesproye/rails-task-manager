578fbc72c1ca25eec5d56881076aec3433861710
Charles Proye <charles.proye@hec.edu>
2020-11-17 12:21:05 +0100

    2nd ex

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  resources :tasks
+
+  #get 'tasks', to: "tasks#index"
+  # New
+  #get 'tasks/new', to: "tasks#new"
+  #Creat
+  #post 'tasks', to: 'tasks#create'
+
+  #get 'tasks/:id', to: "tasks#show", as: :task
+
+  #Edit
+  #get 'tasks/:id/edit', to: 'tasks#edit'
+  #Update
+  #patch 'tasks/:id', to: 'tasks#update'
+
+  #delete 'tasks/:id', to: 'tasks#destroy'
 end
